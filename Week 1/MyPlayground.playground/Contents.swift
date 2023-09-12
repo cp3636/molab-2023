9aa6dc8f089b6015bb3df5106dc0c95ba88ecb6a
cp3636 <cp3636@nyu.edu>
2023-09-12 15:52:11 -0400

    end of day 1

--- Week 1/MyPlayground.playground/Contents.swift
+++ Week 1/MyPlayground.playground/Contents.swift
@@ -74,3 +74,31 @@ print(counter)
 counter *= 2
 counter -= 10
 counter /= 2
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+//store decimal numbers
+//let number = 0.1 + 0.2
+//print(number)
+
+let a = 1
+let b = 2.0
+//let c = a + b
+//cannot combine regular and integer BRO
+let c = a + Int(b)
+
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+//can write
+var Actorname = "Robin Williams"
+Actorname = "Will Ferell"
+//cannot write
+//var name = "Nicholas Cage"
+//name = 50
+
+
