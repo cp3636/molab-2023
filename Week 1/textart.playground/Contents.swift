94fb14891dcec561a5fce541fff5d087b8e0ce5f
cp3636 <cp3636@nyu.edu>
2023-09-14 11:54:26 -0400

    Update

--- Week 1/textart.playground/Contents.swift
+++ Week 1/textart.playground/Contents.swift
@@ -7,14 +7,17 @@ var greeting = "Hello, playground"
 let stem = "-"
 let leaf = "^"
 let petal = "o"
-let wholeflower = "🌸"
+
 
 func createFlower() -> String {
-    return stem + leaf + stem + petal
+    return
 }
 
 for _ in 1...3 {
     let flower = createFlower()
     print(flower)
 }
-print (wholeflower)
+
+
+>(.)__ <(.)__ =(.)__
+ (___/  (___/  (___/
