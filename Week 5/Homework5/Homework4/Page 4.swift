e07563c3e4bddb443920139066b477372b2eab91
cp3636 <cp3636@nyu.edu>
2023-10-12 09:37:24 -0400

    Homework 5

---
+++ Week 5/Homework5/Homework4/Page 4.swift
@@ -0,0 +1,55 @@
+//
+//  Page 4.swift
+//  Homework4
+//
+//  Created by Clarissa Pace on 10/4/23.
+//
+import SwiftUI
+
+struct Page4: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Text("Clarissa's Page")
+                    .foregroundColor(.purple)
+                    .font(.largeTitle)
+                   
+                
+                NavigationLink {
+                    Songs()
+                } label: {
+                    Image(uiImage: UIImage(named: "Music")!)
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                    Text("Songs")
+                        .foregroundColor(.cyan)
+                }
+                NavigationLink {
+                    Dog()
+                } label: {
+                    Image(uiImage: UIImage(named: "Dog")!)
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                    Text("Dog")
+                        .foregroundColor(.pink)
+                }
+                NavigationLink {
+                    CountSheep()
+                } label: {
+                    Image(uiImage: UIImage(named: "moon")!)
+                        .resizable()
+                        .frame(width: 90, height: 90)
+                    Text("Bed Time")
+                        .foregroundColor(.blue)
+                }
+            }
+           
+        }
+    }
+}
+
+struct Page4_Previews: PreviewProvider {
+    static var previews: some View {
+        Page4()
+    }
+}
